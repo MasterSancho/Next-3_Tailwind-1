df87d8002bc82de3711c2ae64827d5d0b2ab7b58
Alex M <1maltsevalex@gmail.com>
2021-08-29 14:31:57 +0300

    002-> Layout Component

---
+++ devspace-blog/components/Layout.js
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+
+export default function Layout({ title, keywords, description, children }) {
+ return (
+  <div>
+   <Head>
+    <title>{title}</title>
+    <meta name='keywords' content={keywords} />
+    <meta name='description' content={description} />
+    <link rel='icon' href='/favicon.ico' />
+   </Head>
+
+   <main className='container mx-auto'>{children}</main>
+  </div>
+ );
+}
+
+Layout.defaultProps = {
+ title: 'Welcome to DevSpace',
+ keywords: 'development, coding, programing',
+ description: 'The best info and news in development',
+};
